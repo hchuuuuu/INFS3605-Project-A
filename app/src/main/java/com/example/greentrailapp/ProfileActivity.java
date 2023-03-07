20a267f70466c248a18854e6c713b432b7fac631
hchuuuuu <henry.chu@student.unsw.edu.au>
2023-03-07 23:11:31 +1100

    VER 1.2 QR feature added. Nav bar completed. Primary color changed to green. Fixed login issue.

--- app/src/main/java/com/example/greentrailapp/ProfileActivity.java
+++ app/src/main/java/com/example/greentrailapp/ProfileActivity.java
@@ -10,33 +10,32 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_profile);
         getSupportActionBar().hide();
 
         nav = findViewById(R.id.nav);
-        nav.setSelectedItemId(R.id.explore);
+        nav.setSelectedItemId(R.id.profile);
         //Bottom nav bar navigation
         nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()){
-                    case R.id.explore:
+                    case R.id.profile:
                         return true;
-                    case R.id.qr:
-                        startActivity(new Intent(MainActivity.this, QRActivity.class));
+                    case R.id.explore:
+                        startActivity(new Intent(ProfileActivity.this, MainActivity.class));
                         overridePendingTransition(0,0);
                         return true;
-                    case R.id.profile:
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    case R.id.qr:
+                        startActivity(new Intent(ProfileActivity.this, QRActivity.class));
                         overridePendingTransition(0,0);
                         return true;
 
@@ -44,5 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
     }
 }
---
+++ app/src/main/java/com/example/greentrailapp/QRActivity.java
@@ -0,0 +1,105 @@
+package com.example.greentrailapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.zxing.Result;
+
+public class QRActivity extends AppCompatActivity {
+
+    private CodeScanner mCodeScanner;
+    private CodeScannerView mCodeScannerView;
+    BottomNavigationView nav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        getSupportActionBar().hide();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 123);
+        } else {
+            startScanning();
+        }
+
+        nav = findViewById(R.id.nav);
+        nav.setSelectedItemId(R.id.qr);
+        //Bottom nav bar navigation
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.qr:
+                        return true;
+                    case R.id.explore:
+                        startActivity(new Intent(QRActivity.this, MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(QRActivity.this, ProfileActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+    private void startScanning() {
+
+        mCodeScannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, mCodeScannerView);
+        mCodeScanner.startPreview();   // this line is very important, as you will not be able to scan your code without this, you will only get blank screen
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(QRActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //now if you want to scan again when you click on scanner then do this.
+        mCodeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                startScanning();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
